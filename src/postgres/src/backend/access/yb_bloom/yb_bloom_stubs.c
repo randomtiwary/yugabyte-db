4fbcdf748ecf07abb27689ab1b7d9473331555b4
randomtiwary <296216341+randomtiwary@users.noreply.github.com>
2026-07-05 22:46:23 +0530

    [YSQL] Complete yb_bloom costing, validation, and vacuum hooks

--- src/postgres/src/backend/access/yb_bloom/yb_bloom_stubs.c
+++
@@ -1,6 +0,0 @@
-#include "postgres.h"
-#include "access/yb_bloom.h"
-bool ybbloomvalidate(Oid o) { return true; }
-void ybbloomcostestimate(struct PlannerInfo *r, struct IndexPath *p, double l, Cost *a, Cost *b, Selectivity *c, double *d, double *e) { *a=*b=0; *c=0.01; *d=*e=0; }
-IndexBulkDeleteResult *ybbloombulkdelete(IndexVacuumInfo *i, IndexBulkDeleteResult *s, IndexBulkDeleteCallback c, void *x) { return s; }
-IndexBulkDeleteResult *ybbloomvacuumcleanup(IndexVacuumInfo *i, IndexBulkDeleteResult *s) { return s; }
