154bd1f25a4acc30872775f1731b1cdf6cb32c97
randomtiwary <296216341+randomtiwary@users.noreply.github.com>
2026-07-09 16:24:39 +0530

    YSQL: Re-enable transactional DDL in TestPgRegressPgMiscIndependent
    Enable DDL savepoint support and stop forcing transactional DDL and
    object locking off now that SAVEPOINT/ROLLBACK around DDL is supported.
    
    Test Plan:
    ./yb_build.sh release --java-test org.yb.pgsql.TestPgRegressPgMiscIndependent --skip-cxx-build

--- java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgRegressPgMiscIndependent.java
+++ java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgRegressPgMiscIndependent.java
@@ -35,12 +35,9 @@ public class TestPgRegressPgMiscIndependent extends BasePgRegressTestPorted {
   protected Map<String, String> getTServerFlags() {
     Map<String, String> flagMap = super.getTServerFlags();
     flagMap.put("ysql_sequence_cache_minval", Integer.toString(TURN_OFF_SEQUENCE_CACHE_FLAG));
-    // TODO(#26734): Enable transactional DDL (& table locks) once savepoint for DDLs are supported.
-    flagMap.put("ysql_yb_ddl_transaction_block_enabled", "false");
-    flagMap.put("enable_object_locking_for_table_locks", "false");
-    // Concurrent DDL requires object locking, so keep the two flags consistent.
-    flagMap.put("ysql_enable_concurrent_ddl", "false");
-    flagMap.merge("allowed_preview_flags_csv", "ysql_enable_concurrent_ddl",
+    // Enable DDL savepoints so SAVEPOINT/ROLLBACK around DDL works under txn DDL.
+    flagMap.put("ysql_yb_enable_ddl_savepoint_support", "true");
+    flagMap.merge("allowed_preview_flags_csv", "ysql_yb_enable_ddl_savepoint_support",
         (existing, added) -> existing + "," + added);
     return flagMap;
   }
